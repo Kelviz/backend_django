a40c948aba0c36f3690d85a47f4a32b4db2c1036
Kelviz <mysviurch@gmail.com>
2023-08-15 15:35:17 +0100

    update

--- backend_django/settings.py
+++ backend_django/settings.py
@@ -173,6 +173,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "https://django-r-blog.netlify.app",
+    "https://frontend-react-blog-lilac.vercel.app/",
+   
 ]
 
 
--- blog_api/serializers.py
+++ blog_api/serializers.py
@@ -6,9 +6,6 @@ class PostSerializer(serializers.ModelSerializer):
     author_username = serializers.ReadOnlyField(source='author.username')
 
 
-
-
-
     class Meta:
         model = Post
         fields = ['id', 'title', 'body', 'author', 'author_username',
